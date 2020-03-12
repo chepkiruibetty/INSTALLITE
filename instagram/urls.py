b31526e85ffff9c9ad726cb04c5d7f3a54daedeb
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-12 17:47:57 +0300

    add url for likes

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', views.LogoutView, {"next_page": '/'}),
+    path('accounts/login/', views.LoginView,name='login')
+     
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,22 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.usersignup, name='register_user'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate_account, name='activate'),
-     url(r'^post/$', views.post,name='post'), 
+    url(r'^post/$', views.post,name='post'), 
+    url(r'^image/$', views.image_upload,name='upload'),
+    url(r'^user/$',views.search_user,name='search_user'),
+    url(r'^profile/$', views.profile_info,name='profile'),
+    url(r'^edit/$',views.profile_edit,name='edit'),
+    url(r'^new_comment/(\d+)/$' ,views.add_comment,name='Comments'),
+    url(r'likes/(\d+)/$', views.likes, name="likes"),
+
+
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
