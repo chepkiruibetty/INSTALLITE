518efa79a0350ff353a8896c5163f1234a5da68e
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-12 11:14:09 +0300

    created template for base

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.usersignup, name='register_user'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate_account, name='activate'),
+     url(r'^post/$', views.post,name='post'), 
+    
 ]
