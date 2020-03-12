ecf98e777e9b0c344cf2649d80fe18198e56fe08
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-12 08:23:03 +0300

    activate the sending of email on sign up

--- instagram/settings.py
+++ instagram/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dylankipruto522@gmail.com'
+EMAIL_HOST_PASSWORD = 'leahkipruto08'
+
+EMAIL_PORT = 587
+
---
+++ users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from . models import Image,Profile,Comments
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserSignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=100, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class ImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile','likes','comments','user']
+        
+class ImageProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user',]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user','image','date_posted']
+    
+        
